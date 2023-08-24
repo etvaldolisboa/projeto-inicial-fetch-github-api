fd8f309dfd95292e67c741e4254e87ad20736131
Etvaldo Lisboa <etvaldolisboa@hotmail.com>
2023-08-23 21:42:37 -0400

    alterado o layout para ficar lado a lado

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,16 +8,16 @@ document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
     if (validateEmptyInput(userName)) return
     getUserData(userName)
-
+    
 })
 
 document.getElementById('input-search').addEventListener('keyup', (e) => {
     const userName = e.target.value
     const key = e.which || e.keyCode
     const isEnterKeyPressed = key === 13
-
+    
     if (isEnterKeyPressed) {
-    if (validateEmptyInput(userName)) return
+        if (validateEmptyInput(userName)) return
         getUserData(userName)
     }
 })
@@ -31,15 +31,16 @@ function validateEmptyInput (userName){
 
 async function getUserData(userName) {
     const userResponse = await getUser(userName)
-
+    
 if (userResponse.message === "Not Found") {
     screen.renderNotFound()
     return
 }
     const repositoriesResponse = await getRepositories(userName)
-
+    
     user.setInfo(userResponse)
     user.setRepositories(repositoriesResponse)
     
     screen.renderUser(user)
 }
+getUserData('etvaldolisboa')
